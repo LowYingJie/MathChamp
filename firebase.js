2748ac6d06a41f26ad773160a9d7ac944b5b8cce
LowYingJie <lowyingjie02@gmail.com>
2025-01-09 02:28:02 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,4 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
-  authDomain: "mathchamp-b30de.firebaseapp.com",
-  databaseURL: "https://mathchamp-b30de-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mathchamp-b30de",
-  storageBucket: "mathchamp-b30de.appspot.com",
-  messagingSenderId: "668143144572",
-  appId: "1:668143144572:web:f86bcd48f84d9dc82383f9",
-  measurementId: "G-EX8CWRLTTH",
-};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { firebaseConfig } from "./firebaseConfig.js";
+
+export const app = initializeApp(firebaseConfig);
