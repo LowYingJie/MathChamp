9b33d9b7aba42d7bbc807e8d3efe1c9756db5946
LowYingJie <lowyingjie02@gmail.com>
2025-01-09 01:54:51 +0800

    Update auth.js

---
+++ auth.js
@@ -0,0 +1,3 @@
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
+
+// ... functions for authentication (login, signup, logout) using getAuth()
