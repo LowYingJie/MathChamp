700160e27d66f79e0080a4b515df0ca4be758227
LowYingJie <lowyingjie02@gmail.com>
2025-01-09 02:17:32 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,4 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
-import { firebaseConfig } from "./firebaseConfig.js";
+import { firebaseConfig } from "./firebaseConfig.js"; // Add your Firebase config in firebaseConfig.js
 
 export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
