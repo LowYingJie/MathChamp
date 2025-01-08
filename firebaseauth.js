f9e4fc3429a9be0c07767a91ebc334328cf4cdeb
LowYingJie <lowyingjie02@gmail.com>
2025-01-09 00:11:53 +0800

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,23 +1,21 @@
-
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-//import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTO6IFilO5WuAZyh7bxAl1rLxtrYZFCQ4",
-  authDomain: "lab-equipment-system.firebaseapp.com",
-  databaseURL: "https://lab-equipment-system-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lab-equipment-system",
-  storageBucket: "lab-equipment-system.firebasestorage.app",
-  messagingSenderId: "65659166988",
-  appId: "1:65659166988:web:ecd6584b426470ebb96929",
-  measurementId: "G-VNN22S71HY"
+  apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
+  authDomain: "mathchamp-b30de.firebaseapp.com",
+  projectId: "mathchamp-b30de",
+  storageBucket: "mathchamp-b30de.firebasestorage.app",
+  messagingSenderId: "668143144572",
+  appId: "1:668143144572:web:43f530212a54f1402383f9",
+  measurementId: "G-1SF82Y837L"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
