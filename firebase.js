a6d0066510b0a403631bb03b108dcbeb5e3a557f
Sam125543 <117194866+Sam125543@users.noreply.github.com>
2025-01-09 14:16:19 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID" // Optional
+  apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
+  authDomain: "mathchamp-b30de.firebaseapp.com",
+  databaseURL: "https://mathchamp-b30de-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mathchamp-b30de",
+  storageBucket: "mathchamp-b30de.firebasestorage.app",
+  messagingSenderId: "668143144572",
+  appId: "1:668143144572:web:f86bcd48f84d9dc82383f9"
 };
 
-export default firebaseConfig; // Make it accessible
+export default firebaseConfig;
