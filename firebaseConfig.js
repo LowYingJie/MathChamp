9f7bf2a5b4fbb7e2b5b4183639f2e14bf9b8cb11
LowYingJie <lowyingjie02@gmail.com>
2025-01-09 09:17:42 +0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,24 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
-  authDomain: "mathchamp-b30de.firebaseapp.com",
-  databaseURL: "https://mathchamp-b30de-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mathchamp-b30de",
-  storageBucket: "mathchamp-b30de.firebasestorage.app",
-  messagingSenderId: "668143144572",
-  appId: "1:668143144572:web:f86bcd48f84d9dc82383f9",
-  measurementId: "G-EX8CWRLTTH"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
+    authDomain: "mathchamp-b30de.firebaseapp.com",
+    databaseURL: "https://mathchamp-b30de-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "mathchamp-b30de",
+    storageBucket: "mathchamp-b30de.firebasestorage.app",
+    messagingSenderId: "668143144572",
+    appId: "1:668143144572:web:f86bcd48f84d9dc82383f9",
+    measurementId: "G-EX8CWRLTTH"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
