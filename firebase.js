ec8c1a7d6363dbba4e133370e802c4c4b833fb8b
LowYingJie <lowyingjie02@gmail.com>
2025-01-12 03:25:17 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,16 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCXXHqcCV9kaP7wBVExaqmRv1Fe78X_cx8",
-  authDomain: "mathchamp-b30de.firebaseapp.com",
-  databaseURL: "https://mathchamp-b30de-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mathchamp-b30de",
-  storageBucket: "mathchamp-b30de.firebasestorage.app",
-  messagingSenderId: "668143144572",
-  appId: "1:668143144572:web:f86bcd48f84d9dc82383f9"
-  measurementId: "G-EX8CWRLTTH"
-};
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "./firebaseConfig.js";
 
-export default firebaseConfig;
+let app;
+let analytics;
+
+try {
+  app = initializeApp(firebaseConfig);
+  analytics = getAnalytics(app);
+  console.log("Firebase initialized successfully.");
+} catch (error) {
+  console.error("Firebase initialization error:", error);
+}
+
+export { app, analytics };
